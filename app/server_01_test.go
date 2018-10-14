a5797f69b6237c998ac04ded4a3a6f5e68f9c6ea
Yuma Nemoto <nemoto1257@gmail.com>
2018-10-14 09:11:39 +0900

    add httptest

---
+++ app/server_01_test.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_RoutingWithHttpServer(t *testing.T) {
+	s := CreateServer()
+	server := httptest.NewServer(s)
+	defer server.Close()
+
+	client := server.Client()
+
+	resp, err := client.Get(server.URL + "/hello?name=duck")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(body)
+	expected := "Hello duck."
+	if string(actual) != expected {
+		t.Errorf("got: %s\nwont: %s", actual, expected)
+	}
+}
